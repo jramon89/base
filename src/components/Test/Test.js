498b842e62dc2d63a3aab8c957032eb017a580e3
jramon89 <jramon.roblero@gmail.com>
2017-05-21 17:14:20 -0500

    added prop-types module a style changes

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -1,16 +1,15 @@
-import React, { Component, PrpTypes } from 'react';
+import React, { Component } from 'react';
 import Layout from '../Layout/Layout';
 
 export default
 class Test extends Component{
-
+	
 	render() {
-		const { title, children } = this.props; 
 		return(
 			<Layout
 				title="Test">
 				<h1>
-					Test
+					Tests
 				</h1>				
 			</Layout>
 		);
